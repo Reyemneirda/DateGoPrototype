20f718006fdecc1708eddac0b386f04326b52ebf
Adrien Meyer <reyemneirda@gmail.com>
2018-04-16 10:55:33 +0300

    Initial Commit

---
+++ DateGoPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DateGoPrototype
+//
+//  Created by Adrien Meyer on 16/04/2018.
+//  Copyright © 2018 DateGo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
